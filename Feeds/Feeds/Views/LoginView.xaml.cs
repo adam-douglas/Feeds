a0e64b25587dd0882f874891ae11873fc8b5644a
Adam Douglas <n9439773@qut.edu.au>
2018-09-24 17:50:15 +1000

    Initial Commit

---
+++ Feeds/Feeds/Views/LoginView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Feeds.Models;
+
+namespace Feeds.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginView : ContentPage
+	{
+        public LoginModel loginModel;
+		public LoginView ()
+		{
+			InitializeComponent ();
+            loginModel = new LoginModel();
+            MessagingCenter.Subscribe<LoginModel,string>(this, "LoginAlert",(sender,username) =>
+            {
+                DisplayAlert("Title", username, "Okay");
+            });
+            this.BindingContext = loginModel;
+
+            usernameEntry.Completed += (object sender, EventArgs e) =>
+            {
+                passwordEntry.Focus();
+            };
+
+            passwordEntry.Completed += (object sender, EventArgs e) =>
+            {
+                loginModel.SubmitCommand.Execute(null);
+            };
+        }
+    }
+}
